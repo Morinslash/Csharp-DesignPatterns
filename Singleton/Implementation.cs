169e47f01e1e8c8b4de05691e74f0d71e69478b4
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-09-16 13:27:41 +0200

    Singleton

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,6 +1,23 @@
+using Console = System.Console;
+
 namespace Singleton;
 
-public class Implementation
+public class Logger
 {
-    
+    private static readonly Lazy<Logger> _lazyLogger
+        = new Lazy<Logger>(() => new Logger());
+    // private static Logger? _instance;
+
+    public static Logger Instance => _lazyLogger.Value;
+
+    // public static Logger Instance => _instance ??= new Logger();
+
+    protected  Logger()
+    {
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message}");
+    }
 }
